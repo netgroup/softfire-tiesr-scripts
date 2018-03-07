784b4acfd27868704f1a12235482dfb11cc8d82c
Stefano Salsano <stefano.salsano@uniroma2.it>
2018-03-07 09:59:13 +0100

    setup2.sh

--- srv6router/setup2.sh
+++ srv6router/setup2.sh
@@ -12,5 +12,21 @@ echo "##    The installation process can last many minutes.     ##"
 echo "##   Plase wait and do not interrupt the setup process.   ##"
 echo "############################################################"
 
-echo -e 'MYNAME="$MY_HOST_ID"' > /etc/myhostid
+echo -e "MYNAME=\c" > /etc/myhostid
+echo -e "$MY_HOST_ID" >> /etc/myhostid
 
+if [ -f $MY_HOST_ID.setup ]; then
+  echo -e "Using local file $MY_HOST_ID.setup found in $(pwd)"
+  source $MY_HOST_ID.setup
+else
+  # If cfg file is not present in current folder, we download it
+  echo -e "Local setup file not found in $(pwd). Download from $MGMT"
+  wget $MGMT/$MY_HOST_ID.setup
+  if [ -f $MY_HOST_ID.setup ]; then
+    source $MY_HOST_ID.setup
+  else
+    echo -e "Update Failed...Check Server $MGMT\n"
+    exit 1
+  fi
+fi
+echo -e "Ok!\n"
