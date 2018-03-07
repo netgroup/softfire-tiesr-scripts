1043c2c3cd8dacbb533a62631324bb7e48029666
Stefano Salsano <stefano.salsano@uniroma2.it>
2018-03-07 09:02:08 +0000

    setup2.sh edited online with Bitbucket

--- srv6router/setup2.sh
+++ srv6router/setup2.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
-# The script installs SRv6Router node packages on Linux Ubuntu 17.10
+# This script installs SRv6Router node packages on Linux Ubuntu 17.10
 
 MY_HOST_ID="ADS1"
 MGMT=172.17.0.1
-
-echo -e "\n"
-echo "############################################################"
-echo "##                 SRv6 Router node setup                 ##"
-echo "##                                                        ##"
-echo "##    The installation process can last many minutes.     ##"
-echo "##   Plase wait and do not interrupt the setup process.   ##"
-echo "############################################################"
 
 echo -e "MYNAME=\c" > /etc/myhostid
 echo -e "$MY_HOST_ID" >> /etc/myhostid
@@ -19,7 +11,6 @@ if [ -f $MY_HOST_ID.setup ]; then
   echo -e "Using local file $MY_HOST_ID.setup found in $(pwd)"
   source $MY_HOST_ID.setup
 else
-  # If cfg file is not present in current folder, we download it
   echo -e "Local setup file not found in $(pwd). Download from $MGMT"
   wget $MGMT/$MY_HOST_ID.setup
   if [ -f $MY_HOST_ID.setup ]; then
