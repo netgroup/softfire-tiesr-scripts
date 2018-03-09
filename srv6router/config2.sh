363ecab5b24376f089b8b41aa19ff03350922727
Stefano Salsano <stefano.salsano@uniroma2.it>
2018-03-09 14:31:46 +0000

    config2.sh edited online with Bitbucket

--- srv6router/config2.sh
+++ srv6router/config2.sh
@@ -77,46 +77,7 @@ echoeval sudo ip link set dev $BR_NAME up
 #declare -a VNF2_DEV=(VNF2_DEV1)
 #declare -a VNF2_DEV1=(L3 fd01:f2::fe 32 fd01:f2::1 $VNF_IF)
 
-vnfs_terms_setup () {
-  echo "VNFs and TERMs SETUP"
-  for i in ${VNF[@]}; do
-
-    eval TYPE_VNF_TERM=\${${i}[0]}
-    #echo $TYPE_VNF_TERM
-    eval VNF_NAME=\${${i}[1]}
-    #echo $VNF_NAME
-    eval VNF_DEV=\${${i}[2]}
-    #echo $VNF_DEV
-
-    tmp=$VNF_DEV[@]
-    DEVARRAY=( "${!tmp}" )
-    #echo ${DEVARRAY[@]}
-
-    for j in ${DEVARRAY[@]}; do
-      eval LAYER=\${$DEVARRAY[0]}
-      #echo $LAYER
-      eval IP_GW=\${$DEVARRAY[1]}
-      #echo $IP_GW
-      eval NETMASK=\${$DEVARRAY[2]}
-      #echo $NETMASK
-      eval IP_VNF=\${$DEVARRAY[3]}
-      #echo $IP_VNF
-      eval DEV_NAME=\${$DEVARRAY[4]}
-      #echo $DEV_NAME
-      eval BR_NAME=\${$DEVARRAY[5]}
-      #echo $BR_NAME
-
-      if [ "$TYPE_VNF_TERM" = "lxd" ]; then
-        lxd_container $VNF_NAME $IP_GW $NETMASK $IP_VNF $DEV_NAME $BR_NAME
-      elif [ "$TYPE_VNF_TERM" = "netns" ]; then
-        netns $VNF_NAME $IP_GW $NETMASK $IP_VNF $DEV_NAME $BR_NAME
-      fi
-
-    done
-
-  done
-}
-
+source vnfs_terms_setup.sh
 
 # Create quagga setup
 quagga_setup() {
