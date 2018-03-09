d30436cd8beeae2b94134cdbaf37dab45e1f09e8
Stefano Salsano <stefano.salsano@uniroma2.it>
2018-03-09 18:37:41 +0100

    fix to scripts

--- srv6router/load_cfg_file.sh
+++ srv6router/load_cfg_file.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
 # First step is the download of the right configuration file
-echo -e "\nLooking for a valid configuration file $MYNAME.cfg"
+
+CFG_FOLDER=cfg
+
+echo -e "\nLooking for a valid configuration file $CFG_FOLDER/$MYNAME.cfg"
 # if $MYNAME.cfg does not exist we try to download from the server
 
-if [ -f $MYNAME.cfg ]; then
+if [ -f $CFG_FOLDER/$MYNAME.cfg ]; then
   # If cfg file is present in current folder, use it
-  echo -e "File found in $(pwd)"
-  echo -e "Using local configuration file $MYNAME.cfg"
-  source $MYNAME.cfg
+  echo -e "File found in $(pwd)/$CFG_FOLDER/"
+  echo -e "Using local configuration file $CFG_FOLDER/$MYNAME.cfg"
+  source $CFG_FOLDER/$MYNAME.cfg
 else
   # If cfg file is not present in current folder, we download it
   echo -e "Local configuration file not found in $(pwd)"
   echo -e "Downloading from $MGMT"
-  wget $MGMT/$MYNAME.cfg
-  if [ -f $MYNAME.cfg ]; then
-    source $MYNAME.cfg
+  wget -O $CFG_FOLDER/$MYNAME.cfg $MGMT/$MYNAME.cfg 
+  if [ -f $CFG_FOLDER/$MYNAME.cfg ]; then
+    source $CFG_FOLDER/$MYNAME.cfg
   else
     echo -e "Update Failed...Check Server $MGMT\n"
     exit 1
