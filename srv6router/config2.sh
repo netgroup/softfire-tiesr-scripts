8698c59897343984b602ef95d636cb270824c72f
Stefano Salsano <stefano.salsano@uniroma2.it>
2018-03-09 12:25:14 +0000

    config2.sh edited online with Bitbucket

--- srv6router/config2.sh
+++ srv6router/config2.sh
@@ -11,6 +11,8 @@ echo "############################################################"
 
 source /etc/myhostid
 
+source load_cfg_file.sh 
+
 #INTERF_NAME="eth0"
 # Address of the management server
 MGMT=172.17.0.1
@@ -318,29 +320,6 @@ echo -e "Ok!"
 
 # Get ip address of $INTERF_NAME
 #myip=$(ip a show dev $INTERF_NAME | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
-
-# First step is the download of the right configuration file
-echo -e "\nLooking for a valid configuration file $MYNAME.cfg"
-# if $MYNAME.cfg does not exist we try to download from the server
-
-if [ -f $MYNAME.cfg ]; then
-  # If cfg file is present in current folder, use it
-  echo -e "File found in $(pwd)"
-  echo -e "Using local configuration file $MYNAME.cfg"
-  source $MYNAME.cfg
-else
-  # If cfg file is not present in current folder, we download it
-  echo -e "Local configuration file not found in $(pwd)"
-  echo -e "Downloading from $MGMT"
-  wget $MGMT/$MYNAME.cfg
-  if [ -f $MYNAME.cfg ]; then
-    source $MYNAME.cfg
-  else
-    echo -e "Update Failed...Check Server $MGMT\n"
-    exit 1
-  fi
-fi
-echo -e "Ok!\n"
 
 # Let's setup the interfaces, the configuration depends on
 # the tunneling has been chosen for this node
