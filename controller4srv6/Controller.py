a6e49d9170a07755a8a51b2326fc92938362ca8b
Mahdi <mohammad59mt@gmail.com>
2018-03-14 17:23:48 +0100

    adding the IPv6 addresses into the controller file

--- controller4srv6/Controller.py
+++ controller4srv6/Controller.py
@@ -4,14 +4,14 @@ import os
 
 _SLEEP_TIME = 5
 
-_VNF_1_ADDRESS = ' '
-_VNF_2_ADDRESS = ' '
-_SURREY_2_ADDRESS = ' '
+_VNF_1_ADDRESS = 'fd01:f1::1'
+_VNF_2_ADDRESS = 'fd03:f1::1'
+_SURREY_2_ADDRESS = 'fdf0:0:0:2::2'
 
-_TERMINAL_4_ADDRESS = ' '
+_TERMINAL_4_ADDRESS = 'fd04:1::3'
 
-_ADS_2_DEVICE_NAME = ' '
-_ADS_2_ADDRESS = ' '
+_ADS_2_DEVICE_NAME = 'br1term1@if24'
+_ADS_2_ADDRESS = 'fd02:1::fe'
 
 
 def serve():
