485a4f0557570f1cc0d3137f079aa2f5bf1d4f3f
Paolo Lungaroni <lplung001@gmail.com>
2018-03-15 00:31:49 +0000

    setup_template.sh edited online with Bitbucket

--- srv6router/setup_template.sh
+++ srv6router/setup_template.sh
@@ -4,9 +4,10 @@ echo -e 'MYNAME="{{token_name}}"' > /etc/myhostid
 echo -e 'MGMT={{token_ip_mgmt}}' >> /etc/myhostid
 mkdir /home/ubuntu/tiesr
 mkdir /home/ubuntu/tiesr/cfg
-declare -a DOWNLOAD_FILES=(config2.sh load_cfg_file.sh vnfs_terms_setup.sh)
+declare -a DOWNLOAD_FILES=(config2.sh load_cfg_file.sh vnfs_terms_setup.sh setup.sh)
 for MYFILE in ${DOWNLOAD_FILES[@]}; do
 wget -O /home/ubuntu/tiesr/$MYFILE http://{{token_ip_mgmt}}:4000/static/softfire-tiesr-scripts/srv6router/$MYFILE
 chmod +x /home/ubuntu/tiesr/$MYFILE
 done
 chown ubuntu:ubuntu -R /home/ubuntu/tiesr/ 
+/home/ubuntu/tiesr/setup.sh
