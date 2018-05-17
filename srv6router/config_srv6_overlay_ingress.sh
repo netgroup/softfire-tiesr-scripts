095e563b55d7cbeea33fe662536d1872eb865c42
pierventre <pierventre@hotmail.com>
2018-05-17 09:56:19 +0200

    Merging new functionality - Removes controller folder - Adds copyright banner - Adds license file - Improves README

--- srv6router/config_srv6_overlay_ingress.sh
+++ srv6router/config_srv6_overlay_ingress.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-# The script configures the Ingress node of the SRv6 domain 
+
+##############################################################################################
+# Copyright (C) 2018 Pier Luigi Ventre - (CNIT and University of Rome "Tor Vergata")
+# Copyright (C) 2018 Stefano Salsano - (CNIT and University of Rome "Tor Vergata")
+# www.uniroma2.it/netgroup - www.cnit.it
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# The script configures the Ingress node of the SRv6 domain
+#
+# @author Pier Luigi Ventre <pierventre@hotmail.com>
+# @author Stefano Salsano <stefano.salsano@uniroma2.it> 
 
 # Configure SRv6 policy for traffic term1 -> term2
 # No SRv6 policy needed. Routing daemon does the job. 
--- srv6router/tcpdump_install.sh
+++ srv6router/tcpdump_install.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
-# This script downloads the latest tcpdump which supports printing all fields of SRH icluding Segments_list
-# The inner packet information is printed as well. 
+
+##############################################################################################
+# Copyright (C) 2018 Pier Luigi Ventre - (CNIT and University of Rome "Tor Vergata")
+# Copyright (C) 2018 Stefano Salsano - (CNIT and University of Rome "Tor Vergata")
+# www.uniroma2.it/netgroup - www.cnit.it
+#
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This script downloads the latest tcpdump which supports printing all fields of SRH icluding Segments_list. 
+#
+# @author Pier Luigi Ventre <pierventre@hotmail.com>
+# @author Stefano Salsano <stefano.salsano@uniroma2.it> 
 
 #install dependencies 
 apt-get -y update 
